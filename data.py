23384cb16cffc69a9b8a70bf3449ac258d216816
YuanMing Liu <774166816@qq.com>
2017-08-06 23:55:34 +0800

    update

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'This is the article content.',
+            'author': 'mingsquall',
+            'create_date': '2017-08-06'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'This is the article content.',
+            'author': 'mingsquall',
+            'create_date': '2017-08-06'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'This is the article content.',
+            'author': 'mingsquall',
+        'create_date': '2017-08-06'
+        },
+    ]
+
+    return articles
